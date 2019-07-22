055f366856ba307e4323008e12ae0ad48692b75f
LORDBABUINO <jorge.x7@gmail.com>
2019-07-22 01:34:32 -0300

    Implements jwt and user update

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -18,9 +18,13 @@ class User extends Model {
 
     this.addHook('beforeSave', async user => {
       if (user.password)
-        user.password_hash = await bcrypt.hash(user.password, 8)
+        user.password_hash = await bcrypt.hash(user.password, 12)
     })
     return this
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash)
   }
 }
 
