c1310c59353c20cb3797b6f194a158c5c692f621
LORDBABUINO <jorge.x7@gmail.com>
2019-08-03 02:50:55 -0300

    Sends appointment canceling email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '177879d7f3634c',
+    pass: '0e43d40bada3a5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+}
